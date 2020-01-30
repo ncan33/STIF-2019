6386393d8c4a2b84b560907d9b9bbef18b505a71
Nejat Can <59581492+ncan33@users.noreply.github.com>
2020-01-30 18:04:04 -0500

    Update CODE_MTS_test.ino

--- data_analysis/CODE_MTS_test/CODE_MTS_test.ino
+++ data_analysis/CODE_MTS_test/CODE_MTS_test.ino
@@ -1,13 +1,13 @@
 /*
 
- DATAA strain gauge code
+ S.T.I.F. strain gauge code
  By: Nejat Can
  Date: January 20th, 2020
- License: DATAA by Nejat Can is licensed under a Creative Commons Attribution-NonCommercial 4.0 International License.
+ License: S.T.I.F. by Nejat Can is licensed under a Creative Commons Attribution-NonCommercial 4.0 International License.
 
  This code uses "HX711.h" from the following HX711 library: https://github.com/bogde/HX711
 
- Circuit details for this device can be found at: https://github.com/ncan33/DATAA-2019/tree/master/circuit
+ Circuit details for this device can be found at: https://github.com/ncan33/STIF-2019/tree/master/circuit
 
 */
 
@@ -34,5 +34,4 @@ void loop() {
   Serial.print("Readings: ");
   Serial.print(scale.get_units(), 1); //scale.get_units() returns a float
   Serial.println();
-
 }
